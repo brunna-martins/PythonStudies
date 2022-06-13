0586130d5fdcb2fb610630e5d6247fdc27b4ddbf
Brunna Louise <brunna.louisemr@gmail.com>
2022-06-13 13:53:49 -0300

    ex-update

--- ex0008.py
+++ ex0008.py
@@ -1,1 +1,5 @@
 #Programa que leia valor em metros e o exiba convertido em centímetros e milímetros.
+valorM = float(input('Digite um valor em metros: '))
+valorCm = valorM*100
+valorMm = valorM*1000
+print('Valor em centímetros: {}cm.\nValor em milímetros: {}mm.'.format(valorCm,valorMm))
